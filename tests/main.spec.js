bc29415d62c92b54d25ef7444aa2a15e226c2c85
hidylima <hidy63@gmail.com>
2020-01-16 02:11:18 -0300

    adicionar mocha js chai

---
+++ tests/main.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen blablabla', () => {
+        // espera oque aconteça
+        // Entrada de dados
+        // Espera retornar (4) => true | (3) => broken false
+      });
+    });
+    context('Case 2 ', () => {});
+  });
+
+  describe('Method B', () => {});
+});
